63d6ff6e16234a5cf13234cdb9e3f7e1bceb07e2
dexian <dxe.tang@gmail.com>
2023-12-16 21:15:55 +0100

    wip

---
+++ util.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Fetch issue details
+issue_content=$(gh issue view $ISSUE_NUMBER --json body -q .body)
+
+# Extract the username and dates
+# user=$(echo $issue_content | cut -d ':' -f 1)
+# dates=$(echo $issue_content | cut -d ':' -f 2)
+echo $issue_content
+
+# Update vacation_tracker.yaml
+# Note: This is a very simplistic approach. YAML editing in Bash is non-trivial and may require external tools for complex manipulation.
+# echo "$user: $dates" >> vacation_tracker.yaml
+
+# Commit and push changes
+# git config --global user.email "action@github.com"
+# git config --global user.name "GitHub Action"
+# git add vacation_tracker.yaml
+# git commit -m "Update vacation dates for $user"
+# git push
+
+# Create a pull request
+# gh pr create --title "Vacation update for $user" --body "Updating vacation dates for $user" --base main --head feature/vacation-update-$user
+
