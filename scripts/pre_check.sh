0702f30f490da3a2122d95f230f71ebaae3276ce
dexian <dxe.tang@gmail.com>
2023-12-20 18:03:41 +0100

    update pre_check

--- scripts/pre_check.sh
+++ scripts/pre_check.sh
@@ -7,14 +7,14 @@ input=$(cat)
 len=$(echo "$input" | yq 'keys | length')
 if [ $len -ne 1 ]; then
   echo 'should have just one key'
-  exit 5
+  exit 1
 fi 
 
 key=$( echo "$input" | yq eval 'keys | .[0]' )
 
-if yq e ".${key}" profile.yml > /dev/null; then
+if $(yq e "has(\"${key}\")" profile.yml); then
   echo "pre check pass"
 else
   echo "user doesn't exist"
-  exit 4
+  exit 1
 fi
